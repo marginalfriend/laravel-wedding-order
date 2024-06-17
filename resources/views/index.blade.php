aab37578dc38b801a8d605b0929c2f5bad8f1af3
Abdullah <abdullahpsg@gmail.com>
2024-06-17 11:04:52 +0700

    [NEW] added tailwind

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="{{ asset('../css/app.css') }}" rel="stylesheet">
+	@vite('resources/css/app.css')
+	<title>Home</title>
+</head>
+<body class="flex flex-col justify-center p-10">
+	<h1>Welcome to Wedding Organizer</h1>
+</body>
+</html>
